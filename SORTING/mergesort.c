4dd1c358cb5a02b9daab4bbad4e7421e646d3279
0x2moon <0x2moon@protonmail.com>
2022-08-18 13:47:30 -0400

    18d08d22V6

--- SORTING/mergesort.c
+++ SORTING/mergesort.c
@@ -1,63 +1,86 @@
 #include <stdio.h>
+#define TAM 8
+void mergesort(int v[],int inicio, int final);
+void merge( int v[], int inicio, int meio, int final);
+void print(int v[], int tam);
 
-int mergesort(int arr[], int inicio, int fim)
-{
-    if (inicio < fim)
-    {
-        int meio = inicio + (fim - inicio) / 2;
-        mergesort(arr, inicio, meio);
-        mergesort(arr, meio + 1, fim);
-        merge(arr, inicio, meio, fim);
-    }
+int main(){
+    int v[] = {0,3,6,8,9,10,7,2};
+    print(v, TAM);
+	printf("\n\naaaaaaaaaaa\n\n");
+	mergesort(v, 0, TAM-1);
+	print(v, TAM);
+    return 0;
 }
 
-void merge(int arr[], int inicio, int meio, int fim)
-{
-    int n1 = meio - inicio + 1;
-    int n2 = fim - meio;
-    int subA[n1], subB[n2];
-    for (int i = 0; i < n1; i++)
-    {
-        subA[i] = arr[inicio + i];
-    }
-    for (int i = 0; i < n1; i++)
-    {
-        subB[i] = arr[meio + i];
-    }
-    int i = 0, j = 0, k = 0;
-    while (i < n1 && j < n2)
-    {
-        if (subA[i] <= subB[j])
-        {
-            arr[k] = subA[i];
-            i++;
-        }
+void mergesort(int v[], int inicio, int final){
+	if (inicio < final)
+	{   
+		int meio = inicio + (final - inicio) / 2;
+		mergesort(v, inicio, meio);
+		mergesort(v, meio + 1, final);
+		merge(v, inicio, meio, final);
+	}
+}
 
-        else
-        {
-            arr[k] = subB[j];
-            j++;
-        }
-    }
+void merge( int v[], int inicio, int meio, int final){
 
-    while(j < n2)
-    {
-        arr[k] = subB[j];
-        j++;
-        k++;
-    }
+	int controleesquerda = 0, controledireita = 0, controleprincipal = inicio;
 
-    while(i < n1)
-    {
-        arr[k] = subA[i];
-        k++;
-        i++;
-    }
+	int tamanhoesquerda = meio - inicio + 1;
+	int tamanhodireita  = final - meio;
+
+	int esquerda[tamanhoesquerda];
+	int direita [tamanhodireita];
+
+	for (int i = 0; i < tamanhoesquerda; i++)
+	{
+		esquerda[i] = v[inicio + i];	
+	}
+	
+	for (int i = 0; i < tamanhodireita; i++)
+	{
+		direita[i] = v[meio + 1 + i];
+	}
+
+	while (controleesquerda < tamanhoesquerda && controledireita < tamanhodireita)
+	{
+		if (esquerda[controleesquerda] >= direita[controledireita])
+		{
+			v[controleprincipal] = direita[controledireita];
+			controledireita++;
+		}
+		
+		else
+		{
+			v[controleprincipal] = esquerda[controleesquerda];
+			controleesquerda++;
+		}
+		controleprincipal++;
+	}
+	
+	while (controleesquerda < tamanhoesquerda)
+	{
+		v[controleprincipal] = esquerda[controleesquerda];
+		controleesquerda++;
+		controleprincipal++;
+	}
+	
+
+	while (controledireita < tamanhodireita)
+	{
+		v[controleprincipal] = direita[controledireita];
+		controledireita++;
+		controleprincipal++;
+	}
+
 
 }
 
-int main()
-{
-    int arr[] = {3, 0, 7, 1, 5, 2, 4, 6}, tam = sizeof(arr)/sizeof(arr[0]);
-    mergesort(arr, 0, tam);
+void print(int v[], int tam){
+    for (int i = 0; i < tam; i++)
+    {
+        printf("v[%d] = %d\n", i, v[i]);
+    }
+    
 }
